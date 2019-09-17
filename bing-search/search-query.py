eef076eba307a9aada9f25c07cbff68adbcdd47d
Mutaz Abu Ghazaleh <mutaz.m.abughazaleh@student.uts.edu.au>
2019-09-17 20:20:16 +1000

    hitting the bing custom search api

---
+++ bing-search/search-query.py
@@ -0,0 +1,35 @@
+# %%
+import json
+from pathlib import Path
+import requests
+
+
+#%%
+secrets_file = Path("./bing-search/secrets.json")
+
+if secrets_file.exists() is True:
+    with open(secrets_file, "rt") as json_data: 
+        secrets = json.load(json_data)
+
+#%%
+def getRequest(secrets, query): 
+    request = f"{secrets['endpoint']}/search?q={query}&customconfig={secrets['config_id']}&mkt=en-US"
+    print(request)
+    header = {'Ocp-Apim-Subscription-Key':secrets['subscription_key1']}
+    r = requests.get(request,headers=header)
+    return r
+
+#%%
+r = getRequest(secrets,'training')
+print(r.json)
+
+#%%
+resp = json.loads(r.text)
+# print(json.dumps(resp, indent=2, sort_keys=True))
+
+# %% 
+webPages = resp['webPages']['value']
+for w in webPages: 
+    print(f"id: {'id' in w.keys()}, "
+    f"searchTags :{'searchTags' in w.keys()}")
+#%%
